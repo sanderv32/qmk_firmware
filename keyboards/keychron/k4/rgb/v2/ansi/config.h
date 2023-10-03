fe4356e2eca755fff4ed1c74f8c2ec3c2b8dd1d0
Philipp Bontemps <ephb@users.noreply.github.com>
2023-10-03 10:53:14 +0200

    [Keyboard] Keychron K4v2 fixes (#352)
    * ISO: Fix Rgb on # key, switch to QK_Boot keycode
    
    * ANSI: Fix build, disable rgb when suspended, enable keypress and framebuffer effects, switch to QK_boot keycode

--- keyboards/keychron/k4/rgb/v2/ansi/config.h
+++ keyboards/keychron/k4/rgb/v2/ansi/config.h
@@ -15,8 +15,14 @@
 #define DIP_SWITCH_PINS { D5, D6 }
 #define RGB_MATRIX_LED_COUNT 100
 
+/* RGB Matrix Animation modes. Explicitly enabled
+ * For full list of effects, see:
+ * https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
+ */
 
+#    define RGB_MATRIX_KEYPRESSES
+#    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
 
+#define RGB_MATRIX_ROW_PINS { C3, C1, C0, C6, C5, C4, C9, C8, C7, C12, C11, C10, B13, C14, C13, B14, B15, D3 }
 
-#define LED_MATRIX_ROW_PINS { C3, C1, C0, C6, C5, C4, C9, C8, C7, C12, C11, C10, B13, C14, C13, B14, B15, D3 }
-
+#define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
