a6bcc9caacad06883b9bc2e42f203a77478451e3
Dimitris Mantzouranis <d3xter93@gmail.com>
2024-01-02 12:56:01 +0200

    RGB Matrix: move SonixQMK headers for drivers

--- quantum/rgb_matrix/rgb_matrix_drivers.h
+++ quantum/rgb_matrix/rgb_matrix_drivers.h
@@ -31,6 +31,10 @@
 #    include "snled27351.h"
 #elif defined(RGB_MATRIX_WS2812)
 #    include "ws2812.h"
+#elif defined(RGB_MATRIX_SN32F24XB)
+#    include "sn32f24xb.h"
+#elif defined(RGB_MATRIX_SLED1734X)
+#    include "sled1734x.h"
 #endif
 
 typedef struct {
