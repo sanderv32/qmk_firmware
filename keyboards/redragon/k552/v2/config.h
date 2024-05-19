d4297be4f078899e47d7e0e12aa6b8cf0605efce
reichi <sreichholf@users.noreply.github.com>
2024-05-19 11:25:54 +0200

    Bring up redragon k552 v2 (ISO-German) (#366)
    * Bring up redragon k552 v2 (ISO-German)
    
    * K552v2: Fix Scroll-Lock
    
    * k552/v2: implement most of the feedback of PR #366
    * fix led matrix layout
    * remove code for empty via layers
    * restructure folder layout (move ISO stuff to iso subfolder) and consolidate some config.h defines
    * rename keyboard to K552 Kumara ISO
    
    * Add copyright, remove temporary comment
    
    * redragon/k552/v2: fix via k552_iso.json
    
    * k552/v2: cleanup config.h/rules.mk structuree (implement review feedback)

---
+++ keyboards/redragon/k552/v2/config.h
@@ -0,0 +1,30 @@
+/* Copyright 2020 Adam Honse <calcprogrammer1@gmail.com>
+ * Copyright 2023 Stephan Reichholf <stephan@reichholf.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define SN32_RGB_MATRIX_ROW_PINS { C0, C1, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, B6, B7, B8, B9, B10, B11 }
+#define SN32_PWM_OUTPUT_ACTIVE_LEVEL SN32_PWM_OUTPUT_ACTIVE_HIGH
+#define SN32_RGB_OUTPUT_ACTIVE_LEVEL SN32_RGB_OUTPUT_ACTIVE_LOW
+#define MATRIX_UNSELECT_DRIVE_HIGH
+
+#define WEAR_LEVELING_SN32_EMULATION_PAGE_COUNT 30
+
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
+#define RGB_MATRIX_TYPING_HEATMAP_DECREASE_DELAY_MS 50
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
---
+++ keyboards/redragon/k552/v2/iso/readme.md
@@ -0,0 +1,17 @@
+# Redragon K552 Kumara ISO
+
+K552 ISO
+
+Keyboard Maintainer: [Stephan Reichholf ](https://github.com/sreichholf)
+Based on Kemove DK63 by: [Stephen Peery](https://github.com/smp4488)
+Hardware Supported: SN32F248BF
+
+Make example for this keyboard (after setting up your build environment):
+
+    make redragon/k552/v2/iso:default
+
+For VIA support compile with:
+
+    make redragon/k552/v2/iso:via
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
