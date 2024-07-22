b773ec3cbb958a07c608cc61b427a1bbd6c1a7af
1Conan <me@1conan.com>
2024-07-22 17:00:22 +0800

    call send functions

--- drivers/bluetooth/bluetooth.c
+++ drivers/bluetooth/bluetooth.c
@@ -21,6 +21,8 @@
 #    include "bluefruit_le.h"
 #elif defined(BLUETOOTH_RN42)
 #    include "rn42.h"
+#elif defined(BLUETOOTH_ITON_BT)
+#    include "iton_bt.h"
 #endif
 
 void bluetooth_init(void) {
@@ -28,6 +30,8 @@ void bluetooth_init(void) {
     bluefruit_le_init();
 #elif defined(BLUETOOTH_RN42)
     rn42_init();
+#elif defined(BLUETOOTH_ITON_BT)
+    iton_bt_init();
 #endif
 }
 
@@ -42,6 +46,8 @@ void bluetooth_send_keyboard(report_keyboard_t *report) {
     bluefruit_le_send_keyboard(report);
 #elif defined(BLUETOOTH_RN42)
     rn42_send_keyboard(report);
+#elif defined(BLUETOOTH_ITON_BT)
+    iton_bt_send_keyboard(report);
 #endif
 }
 
@@ -58,5 +64,7 @@ void bluetooth_send_consumer(uint16_t usage) {
     bluefruit_le_send_consumer(usage);
 #elif defined(BLUETOOTH_RN42)
     rn42_send_consumer(usage);
+#elif defined(BLUETOOTH_ITON_BT)
+    iton_bt_send_consumer(usage)
 #endif
 }
