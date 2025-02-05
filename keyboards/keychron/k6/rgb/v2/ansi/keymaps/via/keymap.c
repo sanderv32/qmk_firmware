6f97ef9fc20837fa8aad7d5af1a7dcaeb4b5a1e1
Alexander Verhaar <sanderv32@gmail.com>
2025-02-05 08:13:00 +0100

    Change keymapping

--- keyboards/keychron/k6/rgb/v2/ansi/keymaps/via/keymap.c
+++ keyboards/keychron/k6/rgb/v2/ansi/keymaps/via/keymap.c
@@ -32,7 +32,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │Ctrl│GUI │Alt │         Space          │Ctl│Fn1│Fn2│ ← │ ↓ │ → │
      * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
      */
-    [WIN_BASE] = LAYOUT_ansi(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, RGB_MOD, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BACKSLASH, KC_HOME, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGUP, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_PGDN, KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RCTL, MO(WIN_FN1), MO(FN2), KC_LEFT, KC_DOWN, KC_RGHT),
+    [WIN_BASE] = LAYOUT_ansi( \
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, RGB_MOD, \
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BACKSLASH, KC_HOME, \
+        KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGUP, \
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_PGDN, \
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RCTL, MO(WIN_FN1), MO(FN2), KC_LEFT, KC_DOWN, KC_RGHT
+    ),
     /* Windows FN1
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┬───┐
      * │`~ │BRU│BRD│TSK│FLX│VAD│VAI│PRV│PLY│NXT│MTE│VLD│VLU│       │TOG│
@@ -46,7 +52,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │    │    │    │                        │Alt│   │   │   │   │   │
      * └────┴────┴────┴────────────────────────┴───┴───┴───┴───┴───┴───┘
      */
-    [WIN_FN1] = LAYOUT_ansi(KC_GRV, KC_BRIU, KC_BRID, KC_TASK, KC_FLXP, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, _______, RGB_TOG, _______, BT_PROFILE1, BT_PROFILE2, BT_PROFILE3, _______, _______, _______, _______, _______, _______, KC_INS, KC_DEL, KC_END, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_RALT, _______, _______, _______, _______, _______),
+    [WIN_FN1] = LAYOUT_ansi( \
+        KC_GRV, KC_BRIU, KC_BRID, KC_TASK, KC_FLXP, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, _______, RGB_TOG, \
+        _______, BT_PROFILE1, BT_PROFILE2, BT_PROFILE3, _______, _______, _______, _______, _______, _______, KC_INS, KC_DEL, KC_END, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, KC_RALT, _______, _______, _______, _______, _______
+    ),
     /* Mac Base
      * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┬───┐
      * │`~ │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │ = │ Backsp│MOD│
